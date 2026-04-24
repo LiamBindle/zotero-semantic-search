90803bbab02b91c97f8b775f40eb9e5f28fa8ee3
Liam Bindle <liambindle@gmail.com>
2026-04-24 02:43:31 -0700

    started on desktop

---
+++ desktop/src/preload.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  onStatus:  (cb)  => ipcRenderer.on('status', (_e, d) => cb(d)),
+  onLog:     (cb)  => ipcRenderer.on('log',    (_e, line) => cb(line)),
+  openBrowser: (url) => ipcRenderer.invoke('open-browser', url),
+  retryDocker: ()    => ipcRenderer.invoke('retry-docker'),
+  copyLogs:  (text)  => ipcRenderer.invoke('copy-logs', text),
+});
